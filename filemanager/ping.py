b6f2c9ad54cf7b2a8198bc3636de78a1ccf0ede1
ivandudkov <idudkov96@gmail.com>
2023-03-28 17:46:13 +0200

    filetrack and assignsvp algorythms modification

---
+++ filemanager/ping.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from dataclasses import dataclass, field
+
+@dataclass
+class Ping:
+    
+    uid: int
+    filename: str
+    ping_number: int
+    timestamp: float
+    proj: str
+    x_coord: float
+    y_coord: float
+    assigned_svp: str
